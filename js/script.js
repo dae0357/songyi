e44e5297a0af00efd900d14cfae808fa34290391
dae0357 <169123688+dae0357@users.noreply.github.com>
2024-05-07 14:32:45 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// $(".menu li a").click(function(){
+//     var id = $($(this).attr("href"));
+//     var loca = id.offset().top;
+//     console.log(loca);
+//     $("html, body").animate({
+//         scrollTop : loca
+//     }, "slow");
+// });
+
+// defer
+
+$('#top ul li a').click(function(){
+    var id = $($(this).attr("href"));
+        var loca = id.offset().top;
+        console.log(loca);
+        $("html, body").animate({
+            scrollTop : loca
+        }, "slow");
+   
+});
